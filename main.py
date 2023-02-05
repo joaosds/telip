9f69f0556ca02300bd63a728dcbba0f547555dbc
joaosds <joaoaugustosds@gmail.com>
2023-02-05 22:06:06 +0100

    feat: send user ip upon request

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from config import TOKEN, CHAT_ID_LIST
-
+import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """
-    user_id = update.message['chat']['id']
+    user_id = update.effective_chat.id
     if user_id not in CHAT_ID_LIST:
-        print(f'User of id {user_id} tried starting the bot.')
-        update.message.reply_text('010010110101010101010110101000101010111010101011011101010110111100010110')
+        print(f"User of id {user_id} tried starting the bot.")
+        await context.bot.send_message(
+            chat_id=user_id,
+            text="010010110101010101010110101000101010111010101011011101010110",
+        )
 
     else:
-        update.message.reply_text('Ativado.')
-    """
-    print(update.to_dict()['message']['chat']['id'])
-    print(context)
+        res = requests.get("https://ifconfig.me")
+        await context.bot.send_message(chat_id=user_id, text=res.text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application = ApplicationBuilder().token(TOKEN).build()
-
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
-
     application.run_polling()
